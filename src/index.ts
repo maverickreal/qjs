b2f3ac883c72638f8144cb14cfca24e6f918e006
Aditya Saxena <adity09@outlook.com>
2026-04-28 17:56:50 +0530

    refactor: remove dead code and unused variables to clean up index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,6 @@ import express, { Request, Response } from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// CODE SMELL: Empty function
-function doNothing() {
-  // TODO: Implement something
-}
-
 // Middleware
 app.use(express.json());
 
@@ -15,10 +10,6 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello from SonarQube Demo Express TS App!" });
 });
-
-// CODE SMELL: Using 'var' instead of 'const/let'
-var oldSchoolVar = "This is not recommended";
-
 
 // Health check
 app.get("/health", (req: Request, res: Response) => {
@@ -33,16 +24,6 @@ app.post("/echo", (req: Request, res: Response) => {
   }
   res.json({ echoed: data, receivedAt: new Date().toISOString() });
 });
-
-// RELIABILITY: Potential infinite loop or unreachable code
-function checkSomething(val: boolean) {
-  if (val) {
-    return true;
-  } else {
-    return false;
-  }
-  return true; // Unreachable code
-}
 
 // Start server
 app.listen(PORT, () => {
